88b7df0dea112192f6d23182facc44f7c0fe798b
seapage <kontakt@seapage.pl>
2022-10-03 22:53:13 +0200

    lodash

---
+++ src/lodash.ts
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+
+var object = { 'a': [{ 'b': { 'c': 3 } }] };
+ 
+_.get(object, 'a[0].b.c');
+// => 3
+ 
+_.get(object, ['a', '0', 'b', 'c']);
+// => 3
+ 
+_.get(object, 'a.b.c', 'default');
+// => 'default'
